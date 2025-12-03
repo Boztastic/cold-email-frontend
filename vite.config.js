cc4134fd612b7a9cd85310d6d58eb1586c1354af
Boztastic <robert@aviro-group.com>
2025-12-03 15:33:50 +0000

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,
     port: process.env.PORT || 3000,
-    strictPort: false,
-    allowedHosts: 'all'
+    strictPort: false
   },
   preview: {
-    host: '0.0.0.0',
+    host: true,
     port: process.env.PORT || 3000,
-    strictPort: false,
-    allowedHosts: 'all'
+    strictPort: false
   }
 })
